55b873e67f47b0a5a42391ef58d006ff79259650
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-11-08 16:31:13 +0200

    Upgrade script

--- mainscript.js
+++ mainscript.js
@@ -2,42 +2,32 @@
 "use strict";
 
 const box = document.getElementById("box");
-const Unicorncursor = document.getElementById("cursor");
+const unicorncursor = document.getElementById("cursor");
 const count = document.getElementById("count");
 const line = document.getElementById("line");
 let score = 0;
-let coordinateX = getRandomInt(50, 565);
-let coordinateY = getRandomInt(50, 565);
-let coordinateY2 = getRandomInt(50, 565);
+let coord1 = getRandomInt(50, 565);
+let coord2 = getRandomInt(50, 565);
+let coord3 = getRandomInt(50, 565);
 
-Unicorncursor.style.left = coordinateX + 'px';
-Unicorncursor.style.top = coordinateY + 'px';
-Unicorncursor.style.right = coordinateY2 + 'px';
-line.style.left = coordinateX + 'px';
-line.style.top = coordinateY + 'px';
-line.style.right = coordinateY2 + 'px';
+unicorncursor.style.left = coord1 + 'px';
+unicorncursor.style.top = coord2 + 'px';
+unicorncursor.style.right = coord3 + 'px';
+line.style.left = coord1 + 'px';
+line.style.top = coord2 + 'px';
+line.style.right = coord3 + 'px';
 
-Unicorncursor.addEventListener('mousemove', () => {
-    coordinateX = getRandomInt(50, 565);
-    coordinateY = getRandomInt(50, 565);
-    coordinateY2 = getRandomInt(50, 565);
-    Unicorncursor.style.left = coordinateX + 'px';
-    Unicorncursor.style.top = coordinateY + 'px';
-    Unicorncursor.style.right = coordinateY2 + 'px';
-    line.style.left = coordinateX + 'px';
-    line.style.top = coordinateY + 'px';
-    line.style.right = coordinateY2 + 'px';
-    // if(window.matchMedia('screen and (min-width: 300px) and (max-width: 800px)')) {
-    //     coordinateX = getRandomInt(50, 100);
-    //     coordinateY = getRandomInt(50, 100);
-    //     coordinateY2 = getRandomInt(50, 100);
-    //     Unicorncursor.style.left = coordinateX + 'px';
-    //     Unicorncursor.style.top = coordinateY + 'px';
-    //     Unicorncursor.style.right = coordinateY2 + 'px';
-    //     line.style.left = coordinateX + 'px';
-    //     line.style.top = coordinateY + 'px';
-    //     line.style.right = coordinateY2 + 'px';
-    // }
+unicorncursor.addEventListener('mousemove', () => {
+    coord1 = getRandomInt(50, 565);
+    coord2 = getRandomInt(50, 565);
+    coord3 = getRandomInt(50, 565);
+    unicorncursor.style.left = coord1 + 'px';
+    unicorncursor.style.top = coord2 + 'px';
+    unicorncursor.style.right = coord3 + 'px';
+    line.style.left = coord1 + 'px';
+    line.style.top = coord2 + 'px';
+    line.style.right = coord3 + 'px';
+    ifMobileSite();
 })
 
 line.addEventListener('click', () => {
@@ -68,7 +58,7 @@ function updateBg() {
         winText.innerText = "Congratulations! You Catch Unicorn! You Win!";
         score = 0;
         count.style.display = "none";
-        Unicorncursor.style.display = "none";
+        unicorncursor.style.display = "none";
         line.style.display = "none";
         let title = document.getElementById("title");
         title.style.display = "none";
@@ -79,6 +69,20 @@ function updateBg() {
         restartBtn.onclick = function() {
             location.reload();
         }
+    }
+}
+
+function ifMobileSite() {
+    if(window.matchMedia("(max-width: 400px)")) {
+        coord1 = getRandomInt(50, 300);
+        coord2 = getRandomInt(50, 300);
+        coord3 = getRandomInt(50, 25);
+        unicorncursor.style.left = coord1 + 'px';
+        unicorncursor.style.top = coord2 + 'px';
+        unicorncursor.style.right = coord3 + 'px';
+        line.style.left = coord1 + 'px';
+        line.style.top = coord2 + 'px';
+        line.style.right = coord3 + 'px';
     }
 }
 
